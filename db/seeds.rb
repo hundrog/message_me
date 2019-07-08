c7f72b04ed124171c213ffd412f5aff697f3333e
hundrog <reciog.oscar@hotmail.com>
2019-07-08 16:32:11 -0500

    add users and messages controllers and models, display messages in index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "hundrog", password: "password")
+User.create(username: "layla", password: "password")
+User.create(username: "john", password: "password")
+User.create(username: "pepixd", password: "password")
+User.create(username: "wowsito", password: "password")
