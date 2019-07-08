3afe34f18eaec0dd765447ef6a70647e10d75f3d
hundrog <reciog.oscar@hotmail.com>
2019-07-08 17:26:34 -0500

    add links to login and logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save { self.username = username.capitalize }
+  # before_save { self.username = username.capitalize }
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 15 }
