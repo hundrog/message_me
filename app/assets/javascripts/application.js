e82876c9a7ad52faa2bca63d8a611fe71b513c5c
hundrog <reciog.oscar@hotmail.com>
2019-07-09 13:15:28 -0500

    solve javascript error, enable harmony for uglifier

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
 //= require_tree .
 
 scroll_bottom = function () {
-  if ($("#messages").length >= 0) {
+  if ($("#messages").length > 0) {
     $("#messages").scrollTop($("#messages")[0].scrollHeight);
   }
 };
